87fd1a68dbd8df60ab21e6c24f581a89b067ea9f
yorkyinternational-dotcom <yorkyinternational@gmail.com>
2026-02-23 11:55:36 +0530

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/your-repo-name/', // 👈 MUST match your GitHub repository name exactly
-})
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Letschargeev/', // 👈 Add this line!
+
 })
